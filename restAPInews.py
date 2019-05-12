998f036aaf6174ae259ff9be25e818e9d0c8f7b7
Wifelly <supermegawifi@gmail.com>
2019-05-12 13:35:05 +0500

    code optimization

--- restAPInews.py
+++ restAPInews.py
@@ -2,18 +2,18 @@ debug = True
 
 import requests, pprint
 
-def reqw(mode, county, pageSize, page, TOKEN):
+def req(county = 'ru', pageSize = '20', page = '1', TOKEN = None):
     return requests.get(
-    f'https://newsapi.org/v2/{mode}?'
+    f'https://newsapi.org/v2/top-headlines?'
     f'country={country}&'
     f'pageSize={pageSize}&'
     f'page = {page}&'
     f'apiKey = {TOKEN}'
     ) 
 
-def reqwCat(mode, country, category, TOKEN, pageSize, page):
+def reqCat(category, country = 'ru', pageSize = 20, page = 1, TOKEN = None):
     return requests.get(
-    f'https://newsapi.org/v2/{mode}?'
+    f'https://newsapi.org/v2/top-headlines?'
     f'country={country}&'
     f'category={category}&'
     f'pageSize={pageSize}&'
@@ -21,9 +21,9 @@ def reqwCat(mode, country, category, TOKEN, pageSize, page):
     f'apiKey = {TOKEN}'
     ) 
 
-def reqwKeyW(mode, q, country, TOKEN, pageSize, page):
+def reqKeyW(q, country = 'ru', pageSize = 20, page = 1, TOKEN = None):
     return requests.get(
-    f'https://newsapi.org/v2/{mode}?'
+    f'https://newsapi.org/v2/top-headlines?'
     f'q={q}&'
     f'country={country}&'
     f'pageSize={pageSize}&'
@@ -46,8 +46,16 @@ else :
     page = input()
     q = input()
     category = input()
-mode = 'top-headlines'
 
-response = reqwKeyW(mode, country, TOKEN, pageSize, page, q)
+cont = True
+while(cont):
+    print(
+        'enter mode: '
+        '1 - request'
+        '2 - request category'
+        '3 - request key wrd'
+        )
+    response = reqKeyW(country, TOKEN, pageSize, page, q)
+    pprint.pprint(response.json())
 
-pprint.pprint(response.json())
+
