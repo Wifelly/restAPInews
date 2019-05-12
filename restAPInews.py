a54287ff5d9e6821cebc99ace938af815f964c39
Wifelly <supermegawifi@gmail.com>
2019-05-12 12:52:19 +0500

    updates

--- restAPInews.py
+++ restAPInews.py
@@ -15,28 +15,29 @@ import requests, pprint
 def reqwCat(mode, country, TOKEN, pageSize, page):
     return requests.get(
     f'https://newsapi.org/v2/{mode}?'
+    f'q={q}'
     f'country={country}&'
-    f'apiKey={TOKEN}&'
     f'pageSize={pageSize}&'
     f'page = {page}'
+    f'apiKey={TOKEN}&'
     ) 
 
-def reqwKeyW():
+def reqwKeyW(mode, country, TOKEN, pageSize, page, q):
     return requests.get(
     f'https://newsapi.org/v2/{mode}?'
     f'country={country}&'
-    f'apiKey={TOKEN}&'
     f'pageSize={pageSize}&'
     f'page = {page}'
+    f'apiKey={TOKEN}&'
     ) 
 
 def reqw():
     return requests.get(
     f'https://newsapi.org/v2/{mode}?'
     f'country={country}&'
-    f'apiKey={TOKEN}&'
     f'pageSize={pageSize}&'
     f'page = {page}'
+    f'apiKey={TOKEN}&'
     ) 
 
 TOKEN = '34c8180e516a42159122bb8040ce0511'
@@ -45,20 +46,16 @@ if (debug):
     language = 'ru'
     pageSize = '20'
     page = '1'
+    q = 'Путин'
 else :
     country = input()
     language = input()
     pageSize = input()  
     page = input()
+    q = input()
 mode = 'top-headlines'
 # mode = 'everything'
-# request = (
-#     f'https://newsapi.org/v2/{mode}?'
-#     f'country={country}&'
-#     f'apiKey={TOKEN}&'
-#     f'pageSize={pageSize}'
-# )
 
-response = reqwCat(mode, country, TOKEN, pageSize, page)
+response = reqwKeyW(mode, country, TOKEN, pageSize, page, q)
 
 pprint.pprint(response.json())
