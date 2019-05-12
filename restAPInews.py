29627da7f7a7d245c097964502acba06817d0dd0
Wifelly <supermegawifi@gmail.com>
2019-05-12 12:34:02 +0500

    First shitty code

---
+++ restAPInews.py
@@ -0,0 +1,42 @@
+# Реализовать набор функций для работы с API:
+# - Получить последние публикации по списку категорий.
+# - Получить последние публикации по списку ключевых слов.
+# - Поиск публикаций по запросу.
+
+# Функции должны предоставлять возможность пагинации (указания размера и номера страницы) и выбора страны/языка.
+
+# Решение должно быть оформлено в виде репозитория на GitHub,
+# в ответе нужно прикрепить ссылку на репозиторий.
+
+debug = True
+
+import requests, pprint
+
+TOKEN = '34c8180e516a42159122bb8040ce0511'
+if (debug):
+    country = 'ru'
+    language = 'ru'
+    pageSize = '20'
+    page = '1'
+else :
+    country = input()
+    language = input()
+    pageSize = input()  
+    page = input()
+mode = 'top-headlines'
+# mode = 'everything'
+
+
+# request = (
+#     f'https://newsapi.org/v2/{mode}?'
+#     f'country={country}&'
+#     f'apiKey={TOKEN}&'
+#     f'pageSize={pageSize}'
+# )
+response = requests.get(
+    f'https://newsapi.org/v2/{mode}?'
+    f'country={country}&'
+    f'apiKey={TOKEN}&'
+    f'pageSize={pageSize}'
+    ) 
+pprint.pprint(response.json())
